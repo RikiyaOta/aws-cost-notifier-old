d871fafb052a08fffc5545436c140eb4b8e50d86
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-01-21 15:20:32 +0900

    Add a permission to lambda function.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import {App, Stack, StackProps} from 'aws-cdk-lib'
 import { Architecture, Code, Function, Runtime, Tracing } from 'aws-cdk-lib/aws-lambda';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
 
 export class AwsCostNotifierStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
         super(scope, id, props)
 
-        new Function(this, 'aws-cost-notifier-lambda-function', {
+        const lambdaFunction = new Function(this, 'aws-cost-notifier-lambda-function', {
             runtime: Runtime.PROVIDED_AL2,
             architecture: Architecture.ARM_64,
             functionName: 'aws-cost-notifier',
@@ -15,6 +16,12 @@ export class AwsCostNotifierStack extends Stack {
             code: Code.fromAsset(`${__dirname}/../target/lambda/aws-cost-notifier`),
             tracing: Tracing.ACTIVE,
         })
+
+        lambdaFunction.addToRolePolicy(new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: ['ce:GetCostAndUsage'],
+            resources: ['*']
+        }))
     }
 }
 
