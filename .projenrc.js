8bb063a3c23e47145854f68939962c0b3a7236d3
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-01-21 13:17:55 +0900

    Ignore target directory.

--- .projenrc.js
+++ .projenrc.js
@@ -3,6 +3,10 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'aws-cost-notifier',
+  
+  gitignore: [
+    'target/'
+  ],
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
